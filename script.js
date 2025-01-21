237ab2b4ac9f3ba2567f36d5561245f881a394b2
Kent Ezekiel Villahermosa <152951264+kaken03@users.noreply.github.com>
2025-01-21 19:06:51 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+// Get the button element by its ID
+    const button = document.getElementById('myButton');
+
+    // Add an event listener to the button to show an alert when clicked
+    button.addEventListener('click', function() {
+        alert("Hello, welcome to my webpage!");
+    });
